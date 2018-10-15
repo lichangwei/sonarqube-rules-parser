d15577b09ba215405d7e6f5297b040682d109e5b
李长伟 <lichangwei@love.com>
2018-10-15 18:19:58 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,15 +2,15 @@ export default {
     sonarqube: 'https://sonarcloud.io/',
     qprofile: 'AWM-7aN5iprWWeJOistG',
     exclude: [
-        // http://sonar.gaiaworks.cn:9000/coding_rules#rule_key=javascript%3AS1854
+        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AS1854
         'javascript:S1854',
-        // http://sonar.gaiaworks.cn:9000/coding_rules#rule_key=javascript%3AUnusedVariable
+        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AUnusedVariable
         'javascript:UnusedVariable',
-        // http://sonar.gaiaworks.cn:9000/coding_rules#rule_key=javascript%3AS3317
+        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AS3317
         'javascript:S3317',
-        // http://sonar.gaiaworks.cn:9000/coding_rules#rule_key=javascript%3AMissingNewlineAtEndOfFile
+        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AMissingNewlineAtEndOfFile
         'javascript:MissingNewlineAtEndOfFile',
-        // http://sonar.gaiaworks.cn:9000/coding_rules#rule_key=javascript%3AS2138
+        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AS2138
         'javascript:S2138',
     ],
 };
