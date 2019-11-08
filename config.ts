0d28923612326b7c09a35400d97c91d0003055dd
louis.li <louis.li@gaiaworks.cn>
2019-11-08 14:51:31 +0800

    *

--- config.ts
+++ config.ts
@@ -1,16 +1,6 @@
 export default {
-    sonarqube: 'https://sonarcloud.io/',
-    qprofile: 'AWM-7aN5iprWWeJOistG',
-    exclude: [
-        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AS1854
-        'javascript:S1854',
-        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AUnusedVariable
-        'javascript:UnusedVariable',
-        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AS3317
-        'javascript:S3317',
-        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AMissingNewlineAtEndOfFile
-        'javascript:MissingNewlineAtEndOfFile',
-        // https://sonarcloud.io/organizations/lichangwei-github/rules?open=javascript%3AS2138
-        'javascript:S2138',
-    ],
+    host: 'https://sonarcloud.io',
+    token: 'a0c1cca3ef25182ef9554630a6bb2d8c28f9c02e',
+    qprofile: 'AWtQX99IlbSAlFzoChdC',
+    exclude: [],
 };
--- index.ts
+++ index.ts
@@ -1,28 +1,26 @@
-import * as fs from 'fs';
-
 import md2c from 'md2c';
-import * as request from 'request';
-import * as Turndown from 'turndown';
-import * as clipboardy from 'clipboardy';
+import request from 'request';
+import Turndown from 'turndown';
+import clipboard from 'clipboardy';
 
 import config from './config';
-
 // 不转义代码中的某些字符
-Turndown.prototype.escape = (text) => text;
+// @ts-ignore
+Turndown.prototype.escape = (text: string): string => text;
 
 const turndown = new Turndown();
 turndown.addRule('pre', {
     filter: ['pre'],
-    replacement: (content) => '```js\n' + content + '```\n',
+    replacement: (content: string) => '```js\n' + content + '```\n',
 });
 turndown.addRule('h2', {
     filter: ['h2'],
-    replacement: (content) => `### ${content}\n`,
+    replacement: (content: string) => `### ${content}\n`,
 });
 
 request.get(
     {
-        url: `${config.sonarqube}/api/rules/search`,
+        url: `${config.host}/api/rules/search`,
         qs: {
             qprofile: config.qprofile,
             f: 'name,mdDesc,severity',
@@ -32,18 +30,21 @@ request.get(
             statuses: 'READY',
             ps: 500, // 分页
         },
+        auth: { user: config.token },
         json: true,
     },
     function(error, res, body) {
         if (error) return console.error(error);
         // 将每个规则中的描述转成`markdown`格式
-        const rules = body.rules.filter((rule) => !config.exclude.includes(rule.key)).map(function(rule, index) {
-            return `## ${index + 1}. ${rule.name}\n${turndown.turndown(rule.mdDesc)}`;
-        });
+        const rules = body.rules
+            .filter((rule) => !config.exclude.includes(rule.key))
+            .map(function(rule, index: number) {
+                return `## ${index + 1}. ${rule.name}\n${turndown.turndown(rule.mdDesc)}`;
+            });
         //生成`markdown`文件内容
         const markdown = `# JavaScript 代码规范\n\n${rules.join('\n\n')}`;
         //将`markdown`转成`Confluence Wiki Markup`并复制到粘贴板
-        clipboardy.writeSync(md2c(markdown));
+        clipboard.writeSync(md2c(markdown));
         console.log('Confluence wiki markup copied.');
     }
 );
